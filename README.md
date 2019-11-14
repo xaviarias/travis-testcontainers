c7d8592d235a5b6e08065458e6108573c5be9ff4
Xavier Arias <xavier@web3labs.com>
2019-11-14 13:42:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Testcontainers in Travis CI
+===========================
+
+Sample project to validate the execution of a [Testcontainers](https://www.testcontainers.org/)
+test in Travis CI.
+
+The [.travis.yml](./.travis.yml) file has been configured using the Testcontainers
+[reference](https://www.testcontainers.org/supported_docker_environment/continuous_integration/dind_patterns/)
+for running tests inside a Docker container. Another example of a Travis CI config file can be found 
+[here](https://raw.githubusercontent.com/testcontainers/testcontainers-java/master/.travis.yml).  
---
+++ src/test/java/TestNetwork.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class TestNetwork {
+    private static final Logger logger = LoggerFactory.getLogger(TestNetwork.class);
+
+    @Test
+    public void yay() throws Exception {
+        try (
+                final Network network = Network.newNetwork();
+
+                final GenericContainer foo = new GenericContainer()
+                        .withLogConsumer(new Slf4jLogConsumer(logger))
+                        .withNetwork(network)
+                        .withNetworkAliases("foo")
+                        .withExposedPorts(8080)
+                        .withCommand("/bin/sh", "-c", "while true ; do printf 'HTTP/1.1 200 OK\\n\\nyay' | nc -l -p 8080; done")
+        ) {
+            foo.start();
+
+            InputStream inputStream = new URL("http://localhost:" + foo.getMappedPort(8080)).openStream();
+            List<String> yay = IOUtils.readLines(inputStream, StandardCharsets.UTF_8);
+            Assert.assertEquals("yay", yay.get(0));
+        }
+    }
+}
