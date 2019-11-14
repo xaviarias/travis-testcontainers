2f64355241143efdcac6e7cec6ed8307f16a6107
Xavier Arias <xavier@web3labs.com>
2019-11-14 16:13:47 +0000

    Formatting

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DOCKER_HOST=unix:///var/run/docker.sock DOCKER_TLS_VERIFY= docker run -t --rm \
-          -v "$HOME/.gradle":/root/.gradle/ \
-          -v /var/run/docker.sock:/var/run/docker.sock \
-          -v "$(pwd)":"$(pwd)" \
-          -w "$(pwd)" \
-          openjdk:8-jdk-alpine \
-          ./gradlew test --stacktrace --info --no-daemon
+  -v "$HOME/.gradle":/root/.gradle/ \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$(pwd)":"$(pwd)" \
+  -w "$(pwd)" \
+  openjdk:8-jdk-alpine \
+  ./gradlew test --stacktrace --info --no-daemon
